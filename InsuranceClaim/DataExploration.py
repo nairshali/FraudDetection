03c66d274536ef5477c703acf29427436ccfdea0
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-08-26 23:26:44 +0100

    Scale Data and PCA

--- InsuranceClaim/DataExploration.py
+++ InsuranceClaim/DataExploration.py
@@ -34,3 +34,19 @@ colors = ["#0101DF", "#DF0101"]
 
 sns.countplot('Class', data=df, palette=colors)
 plt.title('Class Distributions \n (0: No Fraud || 1: Fraud)', fontsize=14)
+
+
+# scaling and PCA
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale, StandardScaler
+scaler = StandardScaler()
+
+X = df.loc[:, df.columns != 'Class']
+y = df.loc[:,['Class']].values
+
+data = scale(X)
+# data = scaler.fit(X)
+pca = PCA(n_components=10)
+principalComponents = pca.fit_transform(data)
+principalDf = pd.DataFrame(data = principalComponents, columns = ['X1','X2','X3','X4','X5','X6','X7','X8','X9','X10'])
+finalDf = pd.concat([principalDf, df[['Class']]], axis = 1)
