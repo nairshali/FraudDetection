9b0e2bd18262399d631131356bc3df5e01ef3a65
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-08-26 23:21:17 +0100

    Import Library

---
+++ InsuranceClaim/DataExploration.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# read insurance raw data
+df = pd.read_csv("insuranceclaim.csv", sep=",")
+df.head(10)
+
