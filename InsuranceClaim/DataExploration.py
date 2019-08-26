ff412c67a6c51a8c07d629e335a2717c4a1bbe4e
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-08-26 23:24:35 +0100

    Indexed String value

--- InsuranceClaim/DataExploration.py
+++ InsuranceClaim/DataExploration.py
@@ -15,3 +15,13 @@ df['property_damage'] = df['property_damage'].replace("?", 'NO')
 # collision_type
 df.head(10)
 
+
+# find out unique distinct values of each column
+df.nunique()
+
+# drop column having more distinct values
+df = df.drop(["_c39","fraud_reported","policy_number", "policy_bind_date", "insured_zip", "incident_location", "incident_date"], axis=1)
+
+# factorize the string columns and convert it into integer
+for col in df.columns[df.dtypes == object]:
+    df[col] = pd.factorize(df[col])[0]
