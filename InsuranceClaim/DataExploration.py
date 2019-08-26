d9672eb5e4cc18b037f326855215565ec33644dc
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-08-26 23:22:20 +0100

    Fill Missing Value

--- InsuranceClaim/DataExploration.py
+++ InsuranceClaim/DataExploration.py
@@ -6,3 +6,12 @@ import seaborn as sns
 df = pd.read_csv("insuranceclaim.csv", sep=",")
 df.head(10)
 
+df['police_report_available'] = df['police_report_available'].replace("?", 'NO')
+df['collision_type'] = df['collision_type'].replace("?", 'Rear Collision')
+df['property_damage'] = df['property_damage'].replace("?", 'NO')
+
+# df.columns = [col.replace('?', 'NO') for col in df.columns]
+# df.columns = [col.replace('?', 'NO') for col in df.columns]
+# collision_type
+df.head(10)
+
